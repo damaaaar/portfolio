e4d35e533dfa05e1438eb97a309290e91505870d
Daniel Martínez <danielmartinez.damar@hotmail.com>
2023-10-22 21:38:18 -0600

    added 404 page

--- main.jsx
+++ main.jsx
@@ -1,6 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import App from './src/App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './src/routes/ErrorPage/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />
+  }
+])
 
 createRoot(document.getElementById('app')).render(
-  <App />
+  <RouterProvider router={router} />
 )
