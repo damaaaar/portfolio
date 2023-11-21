525b72f9ba303a071155d3c2a1d8311b8892fec8
Daniel Martínez <danielmartinez.damar@hotmail.com>
2023-11-21 17:45:31 -0600

    remove react-router

--- main.jsx
+++ main.jsx
@@ -1,16 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './src/App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import ErrorPage from './src/routes/ErrorPage/index.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />
-  }
-])
 
 createRoot(document.getElementById('app')).render(
-  <RouterProvider router={router} />
+  <App />
 )
