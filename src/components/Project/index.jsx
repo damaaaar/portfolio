3cb9427bcf441b452b07229c93bde73811117f7f
Daniel Martínez <danielmartinez.damar@hotmail.com>
2023-12-01 13:23:32 -0600

    add details to projects section

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -8,21 +8,24 @@ function Project ({ PROJECTS_INFO }) {
       {
         PROJECTS_INFO.map(project => {
           return (
-            <article key={project.id} className='projects-project'>
-              <figure className='project-figure'>
-                <img src={project.image} alt={`${project.title} ilustration`} />
-              </figure>
-              <aside className='project-content'>
-                <SubTitle value={project.title} />
-                <p className='project-description'>
-                  {project.description}
-                </p>
-                <footer className='project-footer'>
-                  <Button address={project.linkLive} value='Live' styles='button project-button' />
-                  <Button address={project.linkCode} value='Code' styles='button project-button' />
-                </footer>
-              </aside>
-            </article>
+            <details key={project.id} className='projects-details'>
+              <summary className='projects-summary'>{project.title}</summary>
+              <article className='projects-project'>
+                <figure className='project-figure'>
+                  <img src={project.image} alt={`${project.title} ilustration`} />
+                </figure>
+                <aside className='project-content'>
+                  <SubTitle value={project.title} />
+                  <p className='project-description'>
+                    {project.description}
+                  </p>
+                  <footer className='project-footer'>
+                    <Button address={project.linkLive} value='Live' styles='button project-button' />
+                    <Button address={project.linkCode} value='Code' styles='button project-button' />
+                  </footer>
+                </aside>
+              </article>
+            </details>
           )
         })
       }
